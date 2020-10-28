a946eae455303b0726e20bade49247544a76c545
ivan0811 <ivanfaathirza@gmail.com>
2020-10-28 11:32:18 +0700

    initial commit

---
+++ src/if1/pkg10119003/latihan18/kelipatan/Kelipatan.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119003.latihan18.kelipatan;
+
+/**
+ *
+ * @author User
+ * NAMA         : Ivan Faathirza
+ * KELAS        : IF1
+ * NIM          : 10119003
+ * Deskripsi Program : program ini untuk menampilkan kelipatan
+ */
+
+public class Kelipatan {
+    static double kelipatan = 3.5;
+    
+    private static void tampilKelipatan(){
+        for(int i = 0; i < 9; i++){
+            kelipatan += 3.5;
+            System.out.println(kelipatan);
+        }
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        tampilKelipatan();        
+    }
+    
+}
